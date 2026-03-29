b86f21a230198ebf79585861ae6f266fd5e915aa
mohammadali27 <amohammad6627@gmail.com>
2026-03-29 23:13:53 +0600

    add navbar and banner

--- src/Componenat/Navbar/Navbar.jsx
+++ src/Componenat/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className=" navbar bg-base-100 shadow-sm sticky top-0 z-50">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,56 +23,78 @@ const Navbar = () => {
                 />{" "}
               </svg>
             </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <h1 className=" text-blue-500 text-3xl font-bold">DigiTools</h1>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="flex gap-5 cursor-pointer">
             <li>
-              <a>Item 1</a>
+              <a href="#products">Products</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a href="#about">Features</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a href="#contact">Pricing</a>
+            </li>
+            <li>
+              <a href="#testimonials">Testimonials</a>
+            </li>
+            <li>
+              <a href="#faq">FAQ</a>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <button className="btn btn-ghost">
+            <img src="https://i.ibb.co.com/Wp7n1Kk9/shopping-cart.png" alt="" />{" "}
+            Login
+          </button>
+          <button className="btn rounded-full btn-primary">Get Started</button>
+        </div>
+      </div>
+      <div className="navbar  flex justify-between items-center ">
+        <div className=" p-5 flex flex-col gap-5">
+          <div className=" flex items-center bg-[#e1e7ff] rounded-full w-fit px-3">
+            <img src="https://i.ibb.co.com/qMhTd0FM/Group-5.png" alt="" />
+            <p className=" text-blue-700  font-bold">
+              New: AI-Powered Tools Available!
+            </p>
+          </div>
+
+          <h1 className="font-bold text-4xl text-black">
+            Supercharge Your <br /> Digital Workflow
+          </h1>
+          <p>
+            Access premium AI tools, design assets, templates, and productivity{" "}
+            <br />
+            software—all in one place. Start creating faster today. <br />{" "}
+            Explore Products
+          </p>
+          <div className=" flex gap-5">
+            <button className="btn btn-primary rounded-full">
+              Explore Products
+            </button>
+            <button className="btn btn-outline rounded-full">
+              <img src="https://i.ibb.co.com/zdjRJJr/Play.png" alt="" />
+              Watch Demo
+            </button>
+          </div>
+        </div>
+        <img src="https://i.ibb.co.com/S7sZrn9M/banner.png" alt="" />
+      </div>
+      <div className=" flex items-center justify-center gap-10 w-full bg-blue-500 h-30 text-white mt-10">
+        <div className="">
+          <h1 className=" font-bold text-4xl">50K+</h1>
+          <p>Active Users</p>
+        </div>
+        <div className="">
+          <h1 className=" font-bold text-4xl">200+</h1>
+          <p>Premium Tools</p>
+        </div>
+        <div className="">
+          <h1 className=" font-bold text-4xl">4.9</h1>
+          <p>Rating</p>
         </div>
       </div>
     </div>
